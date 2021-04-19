469699e1c817be36d9c2a4578f3ba525ea588cc8
Hernan Lopez Vergara <hlopezvg@gmail.com>
2021-04-18 22:16:42 -0500

    Beutifying the response

--- inditex/src/main/java/com/inditex/inditex/controllers/RestController.java
+++ inditex/src/main/java/com/inditex/inditex/controllers/RestController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 
@@ -35,16 +36,11 @@ public class RestController {
             produces = MediaType.APPLICATION_JSON_VALUE)
     List<Price> getPrice(@PathVariable("dateQuery") String dateQuery,
                          @PathVariable("productId") String productId,
-                         @PathVariable("brandId") Long brandId) {
+                         @PathVariable("brandId") Long brandId, HttpServletResponse res) {
         //List<Price> price = priceService.findPriceByProductId(Util.convertStringtoDate(dateQuery), productId, brandId);
         List<Price> price = priceService.findPriceByProductId(Util.convertToDate(dateQuery), productId, brandId);
         if (price.size() <= 0) {
-            // We could also use a custom exception class, for now, I'll leave it as it is.
-            //Aca podemos usar una clase de tipo Exceptions, por ahora lo dejo asi para desmostrar otra
-            //manera
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, "Price not found/Precio no encontrado"
-            );
+            res.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
         return price;
     }
